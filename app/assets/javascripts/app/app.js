185833cf73ac2e8117b166633777e67835f1feb6
makpande <mak_pande@yahoo.com>
2016-06-05 10:57:59 -0400

    first commit

---
+++ app/assets/javascripts/app/app.js
@@ -0,0 +1,18 @@
+angular.module('myApp', ['ngRoute',
+  'ngResource',
+  'myApp.controllers',
+  'myApp.services',
+  'myApp.directives'
+  ])
+  .config(function($routeProvider){
+    $routeProvider.when('/', {
+      templateUrl: '/templates/dashboard.html',
+      controller: 'HomeController',
+      resolve: {
+        session: function(SessionService) {
+          return SessionService.getCurrentUser();
+        }
+      }
+    })
+    .otherwise({redirectTo: '/'});
+  });
